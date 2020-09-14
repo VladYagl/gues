dfa0f13c4dc236cdcea2349924f9f6910edfe9d6
Vlad Yaglamunov <VladYaglamunov@gmail.com>
2020-09-14 20:10:12 +0200

    cock

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("cock");
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const fs = require('fs');
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/videos', (req, res) => {
+        res.send(fs.readFileSync('videos.txt'));
+    })
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
