c4dcafa1d3b7c2f8740b5485c3e93f9231a664e1
Vlad Yaglamunov <VladYaglamunov@gmail.com>
2020-09-17 04:09:39 +0200

    cock

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const fs = require('fs');
 
 express()
-    // .use(bodyParser.text())
+    .use(bodyParser.text())
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    // .get('/edit', (req, res) => res.render('pages/edit'))
+    .get('/edit', (req, res) => res.render('pages/edit'))
 
-    // .get('/videos', (req, res) => {
-    //     res.send(fs.readFileSync('videos.txt'));
-    // })
-    // .post('/videos', (req, res) => {
-    //     fs.writeFileSync('videos.txt', req.body);
-    //     console.log("Videos updated");
-    //     res.sendStatus(200)
-    // })
+    .get('/videos', (req, res) => {
+        res.send(fs.readFileSync('videos.txt'));
+    })
+    .post('/videos', (req, res) => {
+        fs.writeFileSync('videos.txt', req.body);
+        console.log("Videos updated");
+        res.sendStatus(200)
+    })
 
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
